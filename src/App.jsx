7d24df0eec5ebcd89962f16439d9950106e2d13b
DeriaIzzet <deriaizzzet@gmail.com>
2023-11-11 00:44:11 +0200

    Update Router in App file

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
-import { useState } from 'react'
-import './App.css'
+import Topbar from "./components/topbar/Topbar";
+import Homepage from "./pages/homepage/Homepage";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import Settings from "./pages/settings/Settings";
+import Single from "./pages/single/Single";
+import Write from "./pages/write/Write";
+import { BrowserRouter as Router,  Route, Routes } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const currentUser = true;
   return (
-
-  )
+    <Router>
+      <Topbar />
+      <Routes>
+      <Route path="/" element={<Homepage />} /> 
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/post/:id"element={<Single />} />
+        <Route path="/write" element={<Write />} />
+        <Route path="/settings"  element={<Settings />} />  
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
