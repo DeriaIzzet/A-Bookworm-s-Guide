bc05f66cf6cde54862526b91e8982d02e7ebbd34
DeriaIzzet <deriaizzzet@gmail.com>
2023-11-11 21:42:36 +0200

    Update app file

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+import './App.css'
 import Footer from "./components/footer/Footer";
+import Posts from './components/posts/Posts';
+import SinglePost from './components/singlePost/SinglePost';
 import Topbar from "./components/topbar/Topbar";
 import Homepage from "./pages/homepage/Homepage";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
 import Settings from "./pages/settings/Settings";
-import Single from "./pages/single/Single";
+
 import Write from "./pages/write/Write";
 import { BrowserRouter as Router,  Route, Routes } from "react-router-dom";
 
@@ -18,9 +21,10 @@ function App() {
       <Route path="/" element={<Homepage />} /> 
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/post/:id"element={<Single />} />
-        <Route path="/write" element={<Write />} />
+        <Route path="/post"element={<SinglePost />} />
+        <Route path="/create-review" element={<Write />} />
         <Route path="/settings"  element={<Settings />} />  
+        <Route path="/posts"  element={<Posts />} />  
       </Routes>
     </Router>
       <Footer />
