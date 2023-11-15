aa7db88d9ca5d88f130ef84d048fee4f223e6141
DeriaIzzet <deriaizzzet@gmail.com>
2023-11-15 19:13:52 +0200

    Update Homepage

--- client/src/pages/homepage/Homepage.jsx
+++ client/src/pages/homepage/Homepage.jsx
@@ -1,18 +1,27 @@
-import { useLocation } from "react-router";
+// import { useLocation } from "react-router";
 import Header from "../../components/header/Header";
-import Posts from "../../components/posts/Posts";
 import Sidebar from "../../components/sidebar/Sidebar";
 import "./Homepage.css";
+import Post from "../../components/post/Post";
 
-export default function Homepage() {
-  const location = useLocation();
-  console.log(location);
+
+
+export default function Homepage({
+  reviews
+}) {
+  //  const location = useLocation();
+  //  console.log(location);
   return (
     <>
       <Header />
+      
       <div className="home">
-        <Posts />
+      <div className="posts">
+      {reviews.map(x => <Post key={x._id} {...x}/>)}
+    </div>
+    <div>
         <Sidebar />
+    </div>
       </div>
     </>
   );
