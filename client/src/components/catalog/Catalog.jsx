7f01f158ca5e333ed8c7b2a92a1238e60228a4ba
DeriaIzzet <deriaizzzet@gmail.com>
2023-11-15 19:15:36 +0200

    Update Catalog

--- client/src/components/catalog/Catalog.jsx
+++ client/src/components/catalog/Catalog.jsx
@@ -1,24 +1,26 @@
-import SinglePost from "../post/SinglePost";
-import "./Posts.css";
+import SinglePost from "../singlePost/SinglePost";
+import './Catalog.css'
+import pictureHeader from "../../../public/cool-writing.jpeg"
 
-export const  Catalog =({
- reviews
-}) =>{
+
+export default function Catalog({
+  reviews
+}) {
   return (
-    <section>
-      <h2>All Reviews</h2>
-    <div className="allReviews">
-      <div className="reviews">
-        <img src={reviews.img}  />
-        <h4 className="title">Title</h4>
-        <p className="book-review">Book Review</p>
-        <h2 className="genre">genre</h2>
-        <h1 className="rating">ratings</h1>
-        <a href="/details" className="details-btn">Details</a>
+    <section className="catalog-section">
+      <div className="header">
+      <div className="headerTitles"> 
+        <span className="headerTitleLg">All reviews</span>
       </div>
+      <img
+        className="headerImg"
+        src={pictureHeader}
+      />
     </div>
-
-    <h6 className="no-reviews">No Reviews Yet</h6>
+      <div className="catalog-div">
+      {reviews.map(x => <SinglePost key={x._id} {...x}/>)}
+      </div>
+      {reviews.length === 0 && ( <h6 className="no-reviews">No Reviews Yet</h6> ) }
     </section>
   );
 }
