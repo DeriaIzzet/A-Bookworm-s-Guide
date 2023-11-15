13d1ef3eec0bf0b04f5fc16d15c76ec85cc2a7ea
DeriaIzzet <deriaizzzet@gmail.com>
2023-11-15 19:16:16 +0200

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,37 +1,48 @@
 import { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './App.css'
+
+import * as reviewService from "./services/reviewService"
+
 import Footer from "./components/footer/Footer";
-import Posts from './components/posts/Posts';
-import SinglePost from './components/singlePost/SinglePost';
 import Topbar from "./components/topbar/Topbar";
 import Homepage from "./pages/homepage/Homepage";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
 import Settings from "./pages/settings/Settings";
 import Write from "./pages/write/Write";
-import { BrowserRouter as Router,  Route, Routes } from "react-router-dom";
+import Catalog from './components/catalog/Catalog';
+import Single from './pages/single/Single';
+
 
 function App() {
- const [posts, setPosts] = useState([])
- useEffect (()=> {
-  
- })
+  const [reviews, setReviews] = useState([])
+  useEffect(() => {
+    reviewService.getAll()
+      .then(result => {
+        setReviews(result)
+      })
+  }, [])
+
+  const onCreateReviewSubmit = (data) =>{
+    
+  }
   return (
     <>
-    <Router>
-      <Topbar />
-      <Routes>
-      <Route path="/" element={<Homepage />} /> 
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/post"element={<SinglePost />} />
-        <Route path="/create-review" element={<Write />} />
-        <Route path="/settings"  element={<Settings />} />  
-        <Route path="/posts"  element={<Posts />} />  
-      </Routes>
-    </Router>
+      <Router>
+        <Topbar />
+        <Routes>
+          <Route path="/" element={<Homepage reviews={reviews} />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/review" element={< Single />} />
+          <Route path="/catalog" element={<Catalog reviews={reviews} />} />
+          <Route path="/create-review" element={<Write />} />
+          <Route path="/settings" element={<Settings />} />
+        </Routes>
+      </Router>
       <Footer />
-     </>
+    </>
   );
 }
 
