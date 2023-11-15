98a4641a8272f94be64070bf5c37d4015957fad1
DeriaIzzet <deriaizzzet@gmail.com>
2023-11-15 19:15:02 +0200

    Update SinglePost

--- client/src/components/singlePost/SinglePost.jsx
+++ client/src/components/singlePost/SinglePost.jsx
@@ -1,17 +1,41 @@
+import "./SinglePost.css"
+import { Link } from "react-router-dom";
 
-import "./SinglePost.css";
-
-export default function SinglePost() {
+export default function SinglePost({
+  bookTitle, imageUrl, Author, bookGenre, bookReview, bookRating
+}) {
   return (
-    <div className="allReviews">
-    <div className="reviews">
-      <img src={reviews.img}  />
-      <h4 className="title">Title</h4>
-      <p className="book-review">Book Review</p>
-      <h2 className="genre">genre</h2>
-      <h1 className="rating">ratings</h1>
-      <a href="/details" className="details-btn">Details</a>
+    <div className="review-container">
+  
+    <div className="review-details">
+      
+      <div className="detail-item">
+      <div className="detail-item">
+      <Link className="link" to="/review">
+        <img className="detail-content" id="image" src={imageUrl} alt="Book Cover" />   
+          </Link>
+      </div>
+        <p className="detail-content" id="title">{bookTitle}</p>
+      </div>
+      <div className="detail-item">
+        <label>Author:</label>
+        <p className="detail-content" id="review">{Author}</p>
+      </div>
+      {/* <div className="detail-item">
+        <label>Review:</label>
+        <p className="detail-content" id="review">{bookReview}</p>
+      </div> */}
+      <div className="detail-item">
+        <label>Genre:</label>
+        <p className="detail-content" id="genre">{bookGenre}</p>
+      </div>
+      <div className="detail-item">
+        <label>Rating:</label>
+        <p className="detail-content" id="stars">Reader rated this, {bookRating}/5 stars!</p>
+      </div>
+    <a href="/details" className="details-btn">Details</a>
     </div>
   </div>
-  );
-}
+    );
+  }
+  
