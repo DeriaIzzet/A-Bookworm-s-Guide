8e4dc4f25e4e431601f8c66c62e6acf626de72df
DeriaIzzet <deriaizzzet@gmail.com>
2023-11-16 17:07:39 +0200

    update write

--- client/src/pages/write/Write.jsx
+++ client/src/pages/write/Write.jsx
@@ -1,38 +1,56 @@
 import "./Write.css";
+import { useState } from "react";
 
-export default function Write() {
+export default function Write({
+  onCreateReviewSubmit,
+}) {
+  const [values,setValues] = useState({
+    bookTitle: "",
+    imageUrl: "",
+    author: "",
+    bookGenre:"",
+    bookReview: "",
+    bookRating : "",
+  })
+
+  const onChangeHandler = (e) => {
+    setValues(state => ({...state, [e.target.name]: e.target.value}))
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+    onCreateReviewSubmit(values)
+    console.log(values)
+  }
+  
   return (
     <div>
       <h1>Create Review</h1>
       <div className="writePage-div">
-      <form id = "create">
+      <form id="create" onSubmit={onSubmit}>
         <div>
           <label htmlFor="title">Book Title:</label>
-          <input type="text" id="title" />
+          <input value={values.bookTitle} onChange={onChangeHandler} type="text" id="bookTitle" name="bookTitle" placeholder="Enter book title..."/>
+        </div>
+        <div>
+          <label htmlFor="author">Book Author:</label>
+          <input value={values.author} onChange={onChangeHandler} type="text" id="author" name="author" placeholder="Enter book author..."/>
         </div>
         <div>
           <label htmlFor="review">Book Review:</label>
-          <textarea id="review"></textarea>
+          <textarea  value={values.bookReview} onChange={onChangeHandler} name="bookReview" id="bookReview" placeholder="Enter review..."></textarea>
         </div>
         <div>
           <label htmlFor="genre">Book Genre:</label>
-          <select id="genre">
-            <option value="Fiction">Fiction</option>
-            <option value="Thriller">Thriller</option>
-            <option value="Mystery">Mystery</option>
-            <option value="Romance">Romance</option>
-            <option value="Fantasy">Fantasy</option>
-            <option value="Dystopian">Dystopian</option>
-            <option value="Historical">Historical</option>
-          </select>
+          <input value={values.bookGenre} onChange={onChangeHandler} type="text" id="bookGenre" name="bookGenre" placeholder="Enter book genre..."/>
         </div>
         <div>
-        <label for="image">Book Image URL:</label>
-        <input type="url" id="image"/>
+        <label htmlFor="image">Book Image URL:</label>
+        <input value={values.imageUrl} onChange={onChangeHandler} type="url" id="imageUrl" name="imageUrl"  placeholder="Upload photo"/>
       </div>
         <div>
           <label htmlFor="stars">Rating:</label>
-          <input type="number" id="stars" min="0" max="5" />
+          <input value={values.bookRating} onChange={onChangeHandler} type="number" id="bookRating" name="bookRating" min="0" max="5" />
         </div>
         <button type="submit">Submit</button>
       </form>
