ac69a4e1c4ef2a82c4a1cfbe01d87362c0d57a32
DeriaIzzet <deriaizzzet@gmail.com>
2023-12-03 00:02:09 +0200

    update and add functionality to single post

--- client/src/components/singlePost/SinglePost.jsx
+++ client/src/components/singlePost/SinglePost.jsx
@@ -2,29 +2,18 @@ import "./SinglePost.css"
 import { Link } from "react-router-dom";
 
 export default function SinglePost({
-  bookTitle, imageUrl, author, bookGenre, bookReview, bookRating
+  _id,bookTitle, imageUrl,bookGenre ,bookRating
 }) {
   return (
     <div className="review-container">
-  
+
     <div className="review-details">
-      
       <div className="detail-item">
       <div className="detail-item">
-      <Link className="link" to="/review">
         <img className="detail-content" id="image" src={imageUrl} alt="Book Cover" />   
-          </Link>
       </div>
         <p className="detail-content" id="title">{bookTitle}</p>
       </div>
-      <div className="detail-item">
-        <label>Author:</label>
-        <p className="detail-content" id="review">{author}</p>
-      </div>
-      {/* <div className="detail-item">
-        <label>Review:</label>
-        <p className="detail-content" id="review">{bookReview}</p>
-      </div> */}
       <div className="detail-item">
         <label>Genre:</label>
         <p className="detail-content" id="genre">{bookGenre}</p>
@@ -33,9 +22,10 @@ export default function SinglePost({
         <label>Rating:</label>
         <p className="detail-content" id="stars">Reader rated this, {bookRating}/5 stars!</p>
       </div>
-    <a href="/details" className="details-btn">Details</a>
+      <div className="details-btn-div">
+        <Link  to={`/catalog/${_id}`}  className="details-btn" >Details</Link>
+      </div>
     </div>
   </div>
     );
   }
-  
