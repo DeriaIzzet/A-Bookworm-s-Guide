f59f92a1331bb851ba40e421ae8c6ff25d2dc9e3
DeriaIzzet <deriaizzzet@gmail.com>
2023-12-03 00:04:51 +0200

    update post

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -2,35 +2,23 @@ import { Link } from "react-router-dom";
 import "./Post.css";
 
 export default function Post({
-  bookTitle, author ,imageUrl,bookGenre,bookReview,bookRating
+  _id,bookTitle,imageUrl,bookRating
 }) {
   return (
+    
+     
     <div className="reviewHome-container">
-  
     <div className="reviewHome-details">
       
       <div className="detail-item">
       <div className="detail-item">
-      <Link className="link" to="/review">
+      <Link className="link" to={`/catalog/${_id}`}>
         <img className="detail-content" id="image" src={imageUrl} alt="Book Cover" />   
           </Link>
       </div>
         <p className="detail-content" id="title">{bookTitle}</p>
       </div>
-      {/* <div className="detail-item">
-        <label>Author:</label>
-        <p className="detail-content" id="review">{author}</p>
-      </div>
       <div className="detail-item">
-        <label>Review:</label>
-        <p className="detail-content" id="review">{bookReview}</p>
-      </div>
-      <div className="detail-item">
-        <label>Genre:</label>
-        <p className="detail-content" id="genre">{bookGenre}</p>
-      </div> */}
-      <div className="detail-item">
-        {/* <label>Rating:</label> */}
         <p className="detail-content" id="stars">Reader rated this, {bookRating}/5 stars!</p>
       </div>
     </div>
