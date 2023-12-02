baa7731aa0456835c0cd3c306b85941b6f06207a
DeriaIzzet <deriaizzzet@gmail.com>
2023-12-03 00:03:14 +0200

    Add not found component

---
+++ client/src/components/NotFound/NotFound.jsx
@@ -0,0 +1,15 @@
+import "./NotFound.css";
+import notFound from "../../../public/opps not fond.jpg"
+export default function NotFound() {
+  return (
+    
+      <div className="not-found">
+        <img
+        className="not-foundImg"
+        src={notFound}   
+      />
+        <a href="/"  className="not-found-button">Return to Home Page</a>  
+        </div>
+        
+  );
+}
