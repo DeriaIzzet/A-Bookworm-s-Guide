0b57befe74b900ca0fc051acd6eb7ec91575066c
DeriaIzzet <deriaizzzet@gmail.com>
2023-12-02 22:29:27 +0200

    Add functionality to login component

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,17 +1,48 @@
 import "./Login.css";
+import { useContext } from "react";
+
+import { AuthContext } from "../../contexts/AuthContext";
+import { useForm } from "../../hooks/useForm";
 
 export default function Login() {
+  const { onLoginSubmit } = useContext(AuthContext);
+  const { values, changeHandler } = useForm({
+    email: "",
+    password: "",
+  });
   return (
     <div className="login">
       <span className="loginTitle">Login</span>
-      <form className="loginForm">
-        <label>Email</label>
-        <input className="loginInput" type="text" placeholder="Enter your email..." />
-        <label>Password</label>
-        <input className="loginInput" type="password" placeholder="Enter your password..." />
-        <button className="loginButton">Login</button>
+      <form className="loginForm" onSubmit={onLoginSubmit}>
+        <label>Email:</label>
+        <input
+          type="email"
+          className="loginInput"
+          id="email"
+          autoComplete="username"
+          name="email"
+          value={values.email}
+          onChange={changeHandler}
+          placeholder="Enter your email..."
+        />
+
+        <label>Password:</label>
+        <input
+          type="password"
+          className="loginInput"
+          id="login-password"
+          name="password"
+          value={values.password}
+          onChange={changeHandler}
+          autoComplete="current-password"
+        />
+        <input type="submit" className="loginButton" value="Login" />
+        <p className="loginRegisterButton">
+          <span>
+            Don't have profile? Click <a href="/register">here</a>
+          </span>
+        </p>
       </form>
-        <button className="loginRegisterButton">Register</button>
     </div>
   );
 }
