c7a2c8e397c2616567b86f64871940cd0b36ac2a
DeriaIzzet <deriaizzzet@gmail.com>
2023-12-02 22:27:56 +0200

    Add catalog tp pages

---
+++ client/src/pages/catalog/Catalog.jsx
@@ -0,0 +1,28 @@
+import SinglePost from "../../components/singlePost/SinglePost";
+import './Catalog.css'
+import pictureHeader from "../../../public/cool-writing.jpeg"
+
+
+export default function Catalog({
+  reviews
+}) {
+  return (
+    <section className="catalog-section">
+      <div className="header">
+      <div className="headerTitles"> 
+        <span className="headerTitleLg">All reviews</span>
+      </div>
+      <img
+        className="headerImg"
+        src={pictureHeader}
+      />
+    </div>
+      <div className="catalog-div">
+      {reviews.map(x => <SinglePost key={x._id} {...x} />).reverse()}
+      </div>
+      <div className="no-reviews-div">
+      {reviews.length === 0 && ( <h6 className="no-reviews">No Reviews Yet</h6> ) }
+      </div>
+    </section>
+  );
+}
