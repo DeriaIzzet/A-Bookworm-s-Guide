a9cd5f34dc8c03c5d146b21a9c2b2702266c97d1
DeriaIzzet <deriaizzzet@gmail.com>
2023-12-04 00:20:18 +0200

    Add functionality to Topbar

--- client/src/components/topbar/Topbar.jsx
+++ client/src/components/topbar/Topbar.jsx
@@ -1,8 +1,11 @@
 import { Link } from "react-router-dom";
 import "./Topbar.css";
+import { useContext } from "react";
+import { AuthContext } from "../../contexts/AuthContext";
 
 export default function Topbar() {
-  const user = true;
+  const { isAuthenticated, userUsername } = useContext(AuthContext);
+
   return (
     <div className="top">
       <div className="topLeft">
@@ -12,47 +15,42 @@ export default function Topbar() {
         <i className="topIcon fab fa-twitter-square"></i>
       </div>
       <div className="topCenter">
-        <ul className="topList">
-          <li className="topListItem">
-            <Link  to="/">
-              HOME
+        <div className="noUser">
+        <Link to="/">HOME</Link>
+        <Link to="/catalog">ALL REVIEWS</Link>
+        </div>
+        {isAuthenticated && (
+          <div className="topList">
+            <Link to="/create-review">CREATE REVIEW</Link>
+            <Link to="/logout">LOGOUT</Link>
+
+            <Link className="link" to="/settings">
+              <div className="profile">
+                <img
+                  className="topImg"
+                  src="https://images.pexels.com/photos/1858175/pexels-photo-1858175.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+                  alt=""
+                  />
+                  <span>{userUsername}</span>
+                <p>My book list</p>
+                </div>
             </Link>
-          </li>
-          <li className="topListItem">
-            <Link to="/catalog">
-              ALL REVIEWS
-            </Link></li>
-          <li className="topListItem">
-            <Link  to="/create-review">
-             CREATE REVIEW
+          </div>
+        )}
+
+        {!isAuthenticated && (
+         
+            <div className="loginRegister">
+            <Link className="link" to="/login">
+              LOGIN
             </Link>
-          </li>
-          {user && <li className="topListItem">LOGOUT</li>}
-        </ul>
-      </div>
-      <div className="topRight">
-        
-          <Link className="link" to="/settings">
-            <img
-              className="topImg"
-              src="https://images.pexels.com/photos/1858175/pexels-photo-1858175.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
-              alt=""
-            />
-          </Link>
-        
-          <ul className="topList">
-            <li className="topListItem">
-              <Link className="link" to="/login">
-                LOGIN
-              </Link>
-            </li>
-            <li className="topListItem">
-              <Link className="link" to="/register">
-                REGISTER
-              </Link>
-            </li>
-          </ul>
-        <i className="topSearchIcon fas fa-search"></i>
+            <Link className="link" to="/register">
+              REGISTER
+            </Link>
+
+            </div>
+
+        )}
       </div>
     </div>
   );
