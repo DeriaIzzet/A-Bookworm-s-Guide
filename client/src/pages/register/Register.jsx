73cf415c74ed4d851a48dad1e48a4c3de251f1d5
DeriaIzzet <deriaizzzet@gmail.com>
2023-12-04 00:18:39 +0200

    add Functionality to register

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,19 +1,61 @@
-import "./Register.css"
+import "./Register.css";
+
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { useForm } from "../../hooks/useForm";
+import { AuthContext } from "../../contexts/AuthContext";
 
 export default function Register() {
-    return (
-        <div className="register">
+  const { onRegisterSubmit } = useContext(AuthContext);
+  const { values, changeHandler, onSubmit } = useForm(
+    {
+      [RegisterFormKeys.Username]: "",
+      [RegisterFormKeys.Email]: "",
+      [RegisterFormKeys.Password]: "",
+    }.onRegisterSubmit
+  );
+  return (
+    <div className="register">
       <span className="registerTitle">Register</span>
       <form className="registerForm">
         <label>Username</label>
-        <input className="registerInput" type="text" placeholder="Enter your username..." />
+        <input
+          className="registerInput"
+          type="username"
+          id="username"
+          placeholder="Enter username ..."
+          autoComplete="username"
+          name={RegisterFormKeys.Username}
+          value={values[RegisterFormKeys.Username]}
+          onChange={changeHandler}
+        />
         <label>Email</label>
-        <input className="registerInput" type="text" placeholder="Enter your email..." />
+        <input
+          className="registerInput"
+          type="email"
+          id="email"
+          placeholder="Enter email ..."
+          autoComplete="username"
+          name={RegisterFormKeys.Email}
+          value={values[RegisterFormKeys.Email]}
+          onChange={changeHandler}
+        />
         <label>Password</label>
-        <input className="registerInput" type="password" placeholder="Enter your password..." />
+        <input
+          className="registerInput"
+          type="password"
+          id="login-password"
+          autoComplete="current-password"
+          placeholder="Enter password ..."
+          name={RegisterFormKeys.Password}
+          value={values[RegisterFormKeys.Password]}
+          onChange={changeHandler}
+        />
         <button className="registerButton">Register</button>
       </form>
-        <button className="registerLoginButton">Login</button>
+      <span className="registerLoginButton">
+        Already have profile? You can <Link to="/login">Login</Link> here.
+      </span>
     </div>
-    )
+  );
 }
