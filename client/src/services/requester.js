3b44d01ae0d15442119a88b9096635154b416f9e
DeriaIzzet <deriaizzzet@gmail.com>
2023-12-04 00:22:25 +0200

    Update requester

--- client/src/services/requester.js
+++ client/src/services/requester.js
@@ -1,31 +1,46 @@
- const request = async (method, url,data) => { 
-    const options = {}
-    if (method !== 'GET'){
-        options.method = method
+const request = async (method, token, url, data) => {
+    const options = {};
 
-        if(data){
+    if (method !== 'GET') {
+        options.method = method;
+
+        if (data) {
             options.headers = {
-                'content-type' : 'applications/json',
-            }
-            options.body = JSON.stringify(data)
+                'content-type': 'application/json',
+            };
+
+            options.body = JSON.stringify(data);
         }
+    }
+
+    if (token) {
+        options.headers = {
+            ...options.headers,
+            'X-Authorization': token,
+        };
     }
 
     const response = await fetch(url, options);
 
-    try {
-        const result = await response.json();
-        return result
-    } catch (error) {
-        return  {} ;
-        
+    if (response.status === 204) {
+        return {};
     }
 
-    
+    const result = await response.json();
+
+    if (!response.ok) {
+        throw result;
+    }
+
+    return result;
 };
 
-export const get = request.bind(null,'GET')
-export const post = request.bind(null,'POST')
-export const put = request.bind(null,'PUT')
-export const patch = request.bind(null,'PATCH')
-export const del = request.bind(null,'DELETE')
+export const requestMaker = (token) => {
+    return {
+        get: request.bind(null, 'GET', token),
+        post: request.bind(null, 'POST', token),
+        put: request.bind(null, 'PUT', token),
+        patch: request.bind(null, 'PATCH', token),
+        delete: request.bind(null, 'DELETE', token),
+    }
+};
