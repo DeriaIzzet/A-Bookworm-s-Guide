eda84077afbdff26472f6ffb6a6eeae25234dc6e
DeriaIzzet <deriaizzzet@gmail.com>
2023-12-04 00:18:11 +0200

    Add functionality to Login

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -2,22 +2,56 @@ import "./Login.css";
 import { useContext } from "react";
 import { AuthContext } from "../../contexts/AuthContext";
 import { useForm } from "../../hooks/useForm";
+import { Link } from "react-router-dom";
 
-export default function Login() {
-  const {onLoginSubmit} = useContext(AuthContext)
-  const {} = useForm({})
+
+const LoginFormKeys = {
+  Email: 'email',
+  Password: 'password'
+};
+
+export default function Login ()  {
+  const { onLoginSubmit } = useContext(AuthContext);
+  const { values, changeHandler, onSubmit } = useForm({
+      [LoginFormKeys.Email]: '',
+      [LoginFormKeys.Password]: '',
+      [LoginFormKeys.Username]: '',
+  }, onLoginSubmit);
 
   return (
-    <div className="login">
-      <span className="loginTitle">Login</span>
-      <form className="loginForm"  onSubmit={onLoginSubmit} >
-        <label>Email</label>
-        <input className="loginInput" type="text" placeholder="Enter your email..." />
-        <label>Password</label>
-        <input className="loginInput" type="password" placeholder="Enter your password..." />
-        <button className="loginButton">Login</button>
-      </form>
-        <button className="loginRegisterButton">Register</button>
-    </div>
+      <section id="login-page" className="login">
+          <form id="login" method="POST" onSubmit={onSubmit}>
+              <div className="login">
+                  
+                  <h1>Login</h1>
+                  
+                  <label htmlFor="email">Email:</label>
+                  <input
+                      type="email"
+                      id="email"
+                      placeholder="Enter email ..."
+                      autoComplete="username"
+                      name={LoginFormKeys.Email}
+                      value={values[LoginFormKeys.Email]}
+                      onChange={changeHandler}
+                  />
+
+                  <label htmlFor="login-pass">Password:</label>
+                  <input
+                      type="password"
+                      id="login-password"
+                      autoComplete="current-password"
+                      placeholder="Enter password ..."
+                      name={LoginFormKeys.Password}
+                      value={values[LoginFormKeys.Password]}
+                      onChange={changeHandler}
+                  />
+                  <input type="submit" className="btn submit" value="Login" />
+                  <p className="field">
+                      <span className="loginRegisterButton" >Don't have proile?  You can  <Link to="/register"> Register </Link> here. </span>
+                  </p>
+              </div>
+          </form>
+      </section>
   );
 }
