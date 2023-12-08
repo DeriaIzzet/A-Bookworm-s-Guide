cb6d6950409445633998d216686e14bd898931e4
DeriaIzzet <deriaizzzet@gmail.com>
2023-12-08 15:25:43 +0200

    add edit component + functionality

--- client/src/components/edit/Edit.jsx
+++ client/src/components/edit/Edit.jsx
@@ -1,77 +1,102 @@
-import { useState } from "react";
-import "./Edit.css";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import "./Edit.css"
+import { useForm } from "../../hooks/useForm";
+import { useService } from "../../hooks/useService";
+import { reviewServiceMaker } from "../../services/reviewService";
 
+export default function Edit({ OnEditSubmit }) {
+  const { reviewId } = useParams();
+  const reviewService = useService(reviewServiceMaker);
+  const { values, changeHandler, onSubmit, changeValues } = useForm(
+    {
+      bookTitle: "",
+      imageUrl: "",
+      author: "",
+      bookGenre: "",
+      bookReview: "",
+      bookRating: "",
+    },
+    OnEditSubmit
+  );
 
-export default function Edit() {
-    const[values,setValues] = useState ({
-        title,
-    ,)
+  useEffect(() => {
+    reviewService.getOne(reviewId)
+    .then((result) => {
+      changeValues(result);
+    });
+  }, []);
   return (
-    <div className="edit-body">
-      <h1 className="header-h1">Edit Review</h1>
-      <div className="edit-div">
-        <form id="edit" method="post" onSubmit={onSubmit}>
+    <section className="editBackground">
+      <h1 className="headerEdit">Edit Review</h1>
+      <div className="editDiv">
+        <form className="editForm" method="post" onSubmit={onSubmit}>
           <div className="edit">
             <div>
               <label htmlFor="title">Book Title:</label>
               <input
+                value={values.bookTitle}
+                onChange={changeHandler}
                 type="text"
-                id="title"
-                name="title"
-                defaultValue=""
+                id="bookTitle"
+                name="bookTitle"
               />
             </div>
             <div>
               <label htmlFor="author">Book Author:</label>
               <input
+                value={values.author}
+                onChange={changeHandler}
                 type="text"
                 id="author"
                 name="author"
-                defaultValue=""
               />
             </div>
-            <div>
+            <div className="editReview">
               <label htmlFor="review">Book Review:</label>
               <textarea
-                name="review"
-                id="review"
-                defaultValue=""
+                value={values.bookReview}
+                onChange={changeHandler}
+                name="bookReview"
+                id="bookReview"
               ></textarea>
             </div>
             <div>
               <label htmlFor="genre">Book Genre:</label>
               <input
-               
+                value={values.bookGenre}
+                onChange={changeHandler}
                 type="text"
-                id="genre"
-                name="genre"
-                defaultValue=""
+                id="bookGenre"
+                name="bookGenre"
               />
             </div>
             <div>
               <label htmlFor="image">Book Image URL:</label>
               <input
+                value={values.imageUrl}
+                onChange={changeHandler}
                 type="url"
                 id="imageUrl"
                 name="imageUrl"
-                defaultValue=""
               />
             </div>
             <div>
               <label htmlFor="stars">Rating:</label>
               <input
+                value={values.bookRating}
+                onChange={changeHandler}
                 type="number"
-                id="rating"
-                name="rating"
+                id="bookRating"
+                name="bookRating"
                 min="0"
                 max="5"
-                defaultValue=""
               />
             </div>
-            <button type="submit">Submit</button>
+            <button className="submitButton" type="submit">Submit</button>
           </div>
         </form>
       </div>
-    </div>
+      </section>
   );
 }
