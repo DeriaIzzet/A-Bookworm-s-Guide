afbf61a4371e5471419706de88da5a6fcccb2377
DeriaIzzet <deriaizzzet@gmail.com>
2023-12-08 15:27:00 +0200

    update post

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -5,11 +5,8 @@ export default function Post({
   _id,bookTitle,imageUrl,bookRating
 }) {
   return (
-    
-     
     <div className="reviewHome-container">
     <div className="reviewHome-details">
-      
       <div className="detail-item">
       <div className="detail-item">
       <Link className="link" to={`/catalog/${_id}`}>
