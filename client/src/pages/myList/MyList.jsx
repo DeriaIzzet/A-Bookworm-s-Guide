d872c376129827e42d7f7eaefc0fa7bbf4cafe36
DeriaIzzet <deriaizzzet@gmail.com>
2023-12-09 13:13:52 +0200

    Add filtered reviews

--- client/src/pages/myList/MyList.jsx
+++ client/src/pages/myList/MyList.jsx
@@ -6,3 +6,24 @@ import { useAuthContext } from "../../contexts/AuthContext";
 export default function MyList({
   reviews,
 }) {
+  const { userId } = useAuthContext();
+
+  // Filter reviews that match the current user
+  const userReviews = reviews.filter(x => x._ownerId === userId);
+
+  return (
+    <section className="catalog-section">
+      <div className="header">
+      
+      </div>
+      <div className="catalog-div">
+        {/* Render SinglePost for each user's review */}
+        {userReviews.length > 0 ? (
+          userReviews.map(x => <SinglePost key={x._id} {...x} />)
+        ) : (
+          <p className="settingsNo">No reviews yet</p>
+        )}
+      </div>
+    </section>
+  );
+}
