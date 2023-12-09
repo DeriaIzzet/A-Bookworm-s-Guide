5cebca0e1e0754fc52feb438fae3bac80bcb54b2
DeriaIzzet <deriaizzzet@gmail.com>
2023-12-09 13:13:27 +0200

    add MyList component

---
+++ client/src/pages/myList/MyList.jsx
@@ -0,0 +1,8 @@
+
+import SinglePost from "../../components/singlePost/SinglePost";
+import { useAuthContext } from "../../contexts/AuthContext";
+
+
+export default function MyList({
+  reviews,
+}) {
