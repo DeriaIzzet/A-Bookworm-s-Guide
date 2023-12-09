7c1e331fc2a19a536b1090114d1e7013a03edbc2
DeriaIzzet <deriaizzzet@gmail.com>
2023-12-09 13:00:57 +0200

    update post

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -9,9 +9,11 @@ export default function Post({
     <div className="reviewHome-details">
       <div className="detail-item">
       <div className="detail-item">
+     
       <Link className="link" to={`/catalog/${_id}`}>
         <img className="detail-content" id="image" src={imageUrl} alt="Book Cover" />   
           </Link>
+      
       </div>
         <p className="detail-content" id="title">{bookTitle}</p>
       </div>
