6b602113665c39db1e19338b7e26bc09fe48c59d
DeriaIzzet <deriaizzzet@gmail.com>
2023-12-09 16:05:17 +0200

    Completely change Router

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -4,15 +4,8 @@ export default function Footer() {
   return (
     <footer className="footerr">
       <div className="footer-container">
-        <div className="footer-logo">
-        </div>
         <nav className="footer-nav">
-          <ul>
-            <li><a href="/about">About Us</a></li>
-            <li><a href="/contact">Contact Us</a></li>
-            <li><a href="/privacy-policy">Privacy Policy</a></li>
-            <li><a href="/terms-of-service">Terms of Service</a></li>
-          </ul>
+        <p>© 2023 A Bookworm's Guide. All rights reserved.</p>
         </nav>
       </div>
     </footer>
