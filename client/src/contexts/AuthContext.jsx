f34852669964e6373b21e58673382c5a244faf11
DeriaIzzet <deriaizzzet@gmail.com>
2023-12-09 13:04:28 +0200

    Add authProvider

--- client/src/contexts/AuthContext.jsx
+++ client/src/contexts/AuthContext.jsx
@@ -3,3 +3,71 @@ import { useNavigate } from 'react-router-dom';
  import { useLocalStorage } from '../hooks/useLocalStorage'; 
 import { authServiceMaker } from '../services/authService';
 export const AuthContext = createContext();
+export const AuthProvider = ({
+    children,
+}) => {
+    const [auth, setAuth] = useLocalStorage('auth',{});
+    const navigate = useNavigate();
+
+    const authService = authServiceMaker(auth.accessToken)
+
+    const onLoginSubmit = async (data) => {
+        try {
+          const result = await authService.login(data);
+    
+          setAuth(result);
+    
+          navigate("/");
+        } catch (error) {
+          console.log("There is a problem");
+        }
+      };
+
+      const onRegisterSubmit = async (values) => {
+        const { confirmPassword, ...registerData } = values;
+        if (confirmPassword !== registerData.password) {
+            return;
+        }
+    
+        try {
+            const result = await authService.register(registerData);
+    
+            setAuth(result);
+    
+            navigate('/');
+        } catch (error) {
+            console.log('There is a problem');
+        }
+    };
+
+    const onLogout = async () => {
+        await authService.logout();
+   
+       setAuth({});
+     };
+
+    const contextVal = {
+        onLoginSubmit,
+        onRegisterSubmit,
+        onLogout,
+        userId: auth._id,
+        token: auth.accessToken,
+        userEmail: auth.email,
+        userUsername: auth.username,
+        isAuthenticated: !!auth.accessToken,
+      };
+
+    return (
+        <>
+            <AuthContext.Provider value={contextVal}>
+                {children}
+            </AuthContext.Provider>
+        </>
+    );
+};
+
+export const useAuthContext = () => {
+    const context = useContext(AuthContext);
+
+    return context;
+};
